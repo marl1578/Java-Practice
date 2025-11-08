2cc174c23ca9817025b41138769cbe454019cd91
marl1578 <sr1205rs@icloud.com>
2025-11-08 15:22:31 +0900

    上書き修正完了

--- src/practice/LoopHandOn.java
+++ src/practice/LoopHandOn.java
@@ -1,10 +1,19 @@
 package practice;
 
+import java.util.Scanner;
+
 public class LoopHandOn {
 
 	public static void main(String[] args) {
-		for (int i = 3; i <= 20; i += 3) {
-			System.out.println("3の倍数:" + i);
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("0を入力してください:");
+		String inputNum = scanner.nextLine();
+		int inputNum2 = Integer.parseInt(inputNum);
+		while(inputNum2 != 0) {
+			
 		}
+		System.out.println("入力されました。終了します。");
+		scanner.close();
 	}
+	
 }
